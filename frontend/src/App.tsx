5b8978d7e5d191c77d6d1e5df95e7b73a3c02ea4
Seishiro Nagi <goudarudra23@gmail.com>
2024-12-25 18:58:57 +0530

    modified package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ export default function App(){
   <RecoilRoot>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Signup/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/signin" element={<Signin/>}/>
           <Route path="/blog/:id" element={<Blog/>}/>
